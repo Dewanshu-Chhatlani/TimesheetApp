3e95a47bd73711a5e717081486d10e6e2c1cc900
Dewanshu-Chhatlani <dewanshu.chhatlani@diaspark.com>
2019-11-04 11:17:37 +0530

    Added activity type dropdown and fixed flatpickr bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,19 @@
-//= require bootstrap-datepicker
+require("turbolinks").start()
+require("stylesheets/application.css")
+
+import "controllers"
+
+import flatpickr from "flatpickr"
+require("flatpickr/dist/flatpickr.css")
+
+document.addEventListener("turbolinks:load", () => {
+    $( '.flatpickr' ).flatpickr({
+        noCalendar: true,
+      enableTime: true,
+      dateFormat: 'h:i K'
+});
+flatpickr('.flatpikrcal', {
+  enableTime: false,
+  dateFormat: "d-m-Y"
+});
+})
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,26 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// require("stylesheets/application.css")
+
+// import "controllers"
+
+import flatpickr from "flatpickr"
+require("flatpickr/dist/flatpickr.css")
+
+document.addEventListener("turbolinks:load", () => {
+    // debugger
+  
+flatpickr('.flatpickr', {
+    noCalendar: true,
+      enableTime: true,
+      dateFormat: 'h:i K'
+});
+flatpickr('.flatpikrcal', {
+  enableTime: false,
+  dateFormat: "d-m-Y"
+});
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
