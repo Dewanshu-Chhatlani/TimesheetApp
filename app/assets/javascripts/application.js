9e14b186be84ff39f0c867099a6a7a78a6ae4836
Dewanshu-Chhatlani <dewanshu.chhatlani@diaspark.com>
2020-02-12 11:22:33 +0530

    Modified width, height of dropdowns and set defaultDate to today

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,19 +1,19 @@
-require("turbolinks").start()
-require("stylesheets/application.css")
+require("turbolinks").start();
+require("stylesheets/application.css");
 
-import "controllers"
+import "controllers";
 
-import flatpickr from "flatpickr"
-require("flatpickr/dist/flatpickr.css")
+import flatpickr from "flatpickr";
+require("flatpickr/dist/flatpickr.css");
 
 document.addEventListener("turbolinks:load", () => {
-    $( '.flatpickr' ).flatpickr({
-        noCalendar: true,
-      enableTime: true,
-      dateFormat: 'h:i K'
+  $(".flatpickr").flatpickr({
+    noCalendar: true,
+    enableTime: true,
+    dateFormat: "h:i K"
+  });
+  flatpickr(".flatpikrcal", {
+    enableTime: false,
+    dateFormat: "d-m-Y"
+  });
 });
-flatpickr('.flatpikrcal', {
-  enableTime: false,
-  dateFormat: "d-m-Y"
-});
-})
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,44 +3,45 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 // require("stylesheets/application.css")
 
 // import "controllers"
 
-import flatpickr from "flatpickr"
-require("flatpickr/dist/flatpickr.css")
+import flatpickr from "flatpickr";
+require("flatpickr/dist/flatpickr.css");
 
 document.addEventListener("turbolinks:load", () => {
-    // debugger
-    flatpickr('.flatpickrstart', {
-      noCalendar: true,
-        enableTime: true,
-        dateFormat: 'h:i K',
-        onChange: function(selectedDates, dateStr, instance) {
-          document.getElementById("startpick").innerHTML = "";
-        }
-  });
-flatpickr('.flatpickrend', {
+  // debugger
+  flatpickr(".flatpickrstart", {
     noCalendar: true,
-      enableTime: true,
-      dateFormat: 'h:i K',
-      onChange: function(selectedDates, dateStr, instance) {
-        document.getElementById("endpick").innerHTML = "";
-      }
+    enableTime: true,
+    dateFormat: "h:i K",
+    onChange: function(selectedDates, dateStr, instance) {
+      document.getElementById("startpick").innerHTML = "";
+    }
+  });
+  flatpickr(".flatpickrend", {
+    noCalendar: true,
+    enableTime: true,
+    dateFormat: "h:i K",
+    onChange: function(selectedDates, dateStr, instance) {
+      document.getElementById("endpick").innerHTML = "";
+    }
+  });
+  flatpickr(".flatpikrcal", {
+    enableTime: false,
+    dateFormat: "d-m-Y",
+    defaultDate: "today",
+    onChange: function(selectedDates, dateStr, instance) {
+      document.getElementById("datepick").innerHTML = "";
+    }
+  });
 });
-flatpickr('.flatpikrcal', {
-  enableTime: false,
-  dateFormat: "d-m-Y",
-  onChange: function(selectedDates, dateStr, instance) {
-    document.getElementById("datepick").innerHTML = "";
-  }
-});
-})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
